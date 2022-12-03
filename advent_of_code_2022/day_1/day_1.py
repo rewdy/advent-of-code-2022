fa4b99ba8644609feab6fe714658870b16e71196
Andrew Meyer <andrewmeyer@granular.ag>
2022-12-02 18:14:57 -0600

    day 1, a little cleaner

--- advent_of_code_2022/day_1/day_1.py
+++ advent_of_code_2022/day_1/day_1.py
@@ -1,28 +1,23 @@
-from pprint import pprint
-from art import text2art, randart
-from yaspin import yaspin
+from art import text2art
 from advent_of_code_2022.day_1.data import data
 
 
-def get_data():
+def get_max():
     data_as_list = data.split("\n")
-    by_elf = {}
+    list_of_lists = []
     index = 0
     for line in data_as_list:
         if line == "":
+            # empty row, new elf
             index += 1
             continue
-        elf_key = index
-        values = [
-            *by_elf.get(elf_key, {}).get("values", []),
-            int(line)
-        ]
-        total = sum(values)
-        by_elf[elf_key] = {
-            "values": values,
-            "total": total,
-        }
-    return by_elf
+        if len(list_of_lists) < index + 1:
+            list_of_lists.append([int(line)])
+        else:
+            list_of_lists[index] = [*list_of_lists[index], int(line)]
+
+    max_calories = max([sum(items) for items in list_of_lists])
+    return max_calories
 
 
 def main():
@@ -30,22 +25,11 @@ def main():
     print("---")
     print("Day 1 of Advent of Code")
 
-    with yaspin(text="Processing elf calorie data") as spinner:
-        data = get_data()
-        spinner.ok()
+    max_calories = get_max()
 
-    with yaspin(text="Finding the elf who carried the most calories") as spinner:
-        max_calories = 0
-        elf_index = None
-        for elf, elf_data in data.items():
-            if elf_data["total"] > max_calories:
-                max_calories = elf_data["total"]
-                elf_index = elf
-        print(text2art("Done!"))
-        print(f"The elf carrying the most calories is elf number {elf_index + 1}")
-        print("That li'l fella is carrying calories adding up to:")
-        print(text2art(f"{max_calories}"))
-        print(max_calories)
+    print("That li'l fella is carrying calories adding up to:")
+    print(text2art(f"{max_calories}"))
+    print(f"{max_calories} 🎅")
 
 
 if __name__ == "__main__":
