7f259b6658b11203b9a289ae57eed5f98025df97
Andrew Meyer <andrewmeyer@granular.ag>
2022-12-05 13:50:50 -0600

    oops. missed part 2.

--- advent_of_code_2022/day_1/day_1.py
+++ advent_of_code_2022/day_1/day_1.py
@@ -1,8 +1,9 @@
 from art import text2art
+from pprint import pprint
 from advent_of_code_2022.day_1.data import data
 
 
-def get_max():
+def process_data():
     data_as_list = data.split("\n")
     list_of_lists = []
     index = 0
@@ -16,8 +17,22 @@ def get_max():
         else:
             list_of_lists[index] = [*list_of_lists[index], int(line)]
 
+    return list_of_lists
+
+
+def get_max(list_of_lists):
+    # Part 1
     max_calories = max([sum(items) for items in list_of_lists])
     return max_calories
+
+
+def get_max_top_three(list_of_lists):
+    # Part 2
+    summed_sorted = [sum(items) for items in list_of_lists]
+    summed_sorted.sort()
+    summed_sorted.reverse()
+    pprint(summed_sorted[:3])
+    return sum(summed_sorted[:3])
 
 
 def main():
@@ -25,11 +40,13 @@ def main():
     print("---")
     print("Day 1 of Advent of Code")
 
-    max_calories = get_max()
+    data_processed = process_data()
+    max_calories_top_elf = get_max(data_processed)
 
-    print("That li'l fella is carrying calories adding up to:")
-    print(text2art(f"{max_calories}"))
-    print(f"{max_calories} 🎅")
+    print(f"Part 1: The elf carrying the most calories is carrying {max_calories_top_elf} kcal 🎅")
+
+    max_calories_top_three = get_max_top_three(data_processed)
+    print(f"Part 2: The three elves carrying the most calories are carrying {max_calories_top_three} kcal 🎅")
 
 
 if __name__ == "__main__":
